f709dbbd879941dda534de2615b0f6b5bcd34a6e
Alexander Weiss <alex@weissfam.de>
2023-07-26 00:07:45 +0200

    document repoinfo

--- crates/rustic_core/src/commands/repoinfo.rs
+++ crates/rustic_core/src/commands/repoinfo.rs
@@ -10,23 +10,37 @@ use crate::{
 };
 
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
+/// Index information from `repoinfo`
 pub struct IndexInfos {
+    /// Infos about blobs
     pub blobs: Vec<BlobInfo>,
+    /// Infos about blobs in packs marked for deletion
     pub blobs_delete: Vec<BlobInfo>,
+    /// Infos about packs
     pub packs: Vec<PackInfo>,
+    /// Infos about packs marked for deletion
     pub packs_delete: Vec<PackInfo>,
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+/// Information about blobs within `repoinfo`
 pub struct BlobInfo {
+    /// blob type
     pub blob_type: BlobType,
+    /// # of blobs of the type
     pub count: u64,
+    /// Total size saved in the repository of all blobs of the type.
+    ///
+    /// This is the size of the blobs after compression and encryption.
     pub size: u64,
+    /// Total data size of all blobs of the type.
+    ///
+    /// This is the raw size of the blobs without compression or encryption.
     pub data_size: u64,
 }
 
 impl BlobInfo {
-    pub fn add(&mut self, ie: IndexEntry) {
+    pub(crate) fn add(&mut self, ie: IndexEntry) {
         self.count += 1;
         self.size += u64::from(ie.length);
         self.data_size += u64::from(ie.data_length());
@@ -35,15 +49,20 @@ impl BlobInfo {
 
 #[serde_with::apply(Option => #[serde(default, skip_serializing_if = "Option::is_none")])]
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+/// Information about packs within `repoinfo`
 pub struct PackInfo {
+    /// packs of the given blob type
     pub blob_type: BlobType,
+    /// # of packs of the type
     pub count: u64,
+    /// minimal pack size for packs of the type, None, if there is no pack.
     pub min_size: Option<u64>,
+    /// maximal pack size for packs of the type, None, if there is no pack.
     pub max_size: Option<u64>,
 }
 
 impl PackInfo {
-    pub fn add(&mut self, ip: &IndexPack) {
+    pub(crate) fn add(&mut self, ip: &IndexPack) {
         self.count += 1;
         let size = u64::from(ip.pack_size());
         self.min_size = self
@@ -109,15 +128,22 @@ pub(crate) fn collect_index_infos<P: ProgressBars, S: Open>(
 
 #[serde_with::apply(Option => #[serde(default, skip_serializing_if = "Option::is_none")])]
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
+/// Information about repository files
 pub struct RepoFileInfos {
+    /// Repository files
     pub repo: Vec<RepoFileInfo>,
+    /// Hot repository files, if we have a hot/cold repository
     pub repo_hot: Option<Vec<RepoFileInfo>>,
 }
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+/// Information about a repository files of a given [`FileType`]
 pub struct RepoFileInfo {
+    /// The type of the files
     pub tpe: FileType,
+    /// The total # of files
     pub count: u64,
+    /// The total size of all files
     pub size: u64,
 }
 
@@ -132,7 +158,7 @@ pub(crate) fn collect_file_info(be: &impl ReadBackend) -> RusticResult<Vec<RepoF
     Ok(files)
 }
 
-pub fn collect_file_infos<P: ProgressBars, S>(
+pub(crate) fn collect_file_infos<P: ProgressBars, S>(
     repo: &Repository<P, S>,
 ) -> RusticResult<RepoFileInfos> {
     let p = repo.pb.progress_spinner("scanning files...");
